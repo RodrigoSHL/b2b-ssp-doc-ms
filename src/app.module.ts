0202957032b76ad1c539f90facf0cedf1e777586
RodrigoSHL <shl.catalan@gmail.com>
2022-11-07 08:25:47 -0300

    feat: add import in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UtilsModule } from './utils/utils.module';
+import { DocumentModule } from './document/document.module';
 
 @Module({
-  imports: [UtilsModule],
+  imports: [UtilsModule, DocumentModule],
   controllers: [AppController],
   providers: [AppService],
 })
