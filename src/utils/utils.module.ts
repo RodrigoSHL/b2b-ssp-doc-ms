065b9fba209627f1a0649b450734875fbb6166f9
RodrigoSHL <shl.catalan@gmail.com>
2022-11-07 08:25:37 -0300

    feat: create utils module

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,4 +1,9 @@
+import { BigQuery } from '@google-cloud/bigquery';
 import { Module } from '@nestjs/common';
+import { BigQueryService } from './bigquery/bigQuery.service';
 
-@Module({})
+@Module({
+  exports: [BigQuery, BigQueryService],
+  providers: [BigQuery, BigQueryService],
+})
 export class UtilsModule {}
